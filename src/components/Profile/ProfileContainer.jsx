7cabf3655fca592bd8905dd9823d84fc9fea2167
User <maxim88kiev@gmail.com>
2020-12-29 13:24:33 +0200

    234234

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Profile from "./Profile";
-import * as axios from "axios";
-import {setUserProfile} from "../../redux/profile-reducer";
+import {getUserProfile} from "../../redux/profile-reducer";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 
@@ -12,10 +11,7 @@ class ProfileContainer extends React.Component{
         if(!userId){
             userId = 2;
         }
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
-            .then(response => {
-                this.props.setUserProfile(response.data);
-            });
+        this.props.getUserProfile(userId);
     }
 
     render(){
@@ -31,4 +27,4 @@ let mapStateToProps = (state) => ({
 
 let WithUrlContainerComponent = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, {setUserProfile})(WithUrlContainerComponent);
+export default connect(mapStateToProps, {getUserProfile})(WithUrlContainerComponent);
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -45,4 +45,10 @@ const profileReducer = (state = initialState, action) => {
 
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 
+export const getUserProfile = (userId) => (dispatch) => {
+    usersAPI.getProfile(userId).then(response => {
+        dispatch(setUserProfile(response.data));
+    });
+}
+
 export default profileReducer;
